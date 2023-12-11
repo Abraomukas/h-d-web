5b063b38e9c2e00704d263fbabb15ae72519b4f3
Abraomukas <abraomukas@gmail.com>
2023-12-11 23:58:58 +0100

    feat: Add login button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Cookies from 'js-cookie';
 import i18next from 'i18next';
 
 const languages = [
-	{ name: 'English', country_code: 'gb' },
 	{ name: 'Español', country_code: 'es' },
 	{ name: 'Deutsch', country_code: 'de' },
 ];
@@ -35,12 +34,12 @@ function Navbar(props) {
 					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
 						<li className='nav-item'>
 							<a className='nav-link' href='#'>
-								Offices
+								Oficinas
 							</a>
 						</li>
 						<li className='nav-item'>
 							<a className='nav-link' href='#'>
-								Holidays
+								Vacaciones
 							</a>
 						</li>
 					</ul>
@@ -60,42 +59,57 @@ function Navbar(props) {
 				</div>
 			</div>
 			<div className='navbar-right'>
-				{/* LANGUAGES */}
-				<div className='dropdown mx-3'>
-					<a
-						className='text-reset me-3 dropdown-toggle hidden-arrow'
-						href='#'
-						id='navbarDropdownMenuLink'
-						role='button'
-						data-mdb-toggle='dropdown'
-						aria-expanded='false'>
-						<i className='fas fa-globe' style={{ color: '#000000' }}></i>
-					</a>
-					<ul
-						className='dropdown-menu dropdown-menu-end'
-						aria-labelledby='navbarDropdownMenuLink'>
-						{languages.map(({ name, country_code, index }) => {
-							return (
-								<li key={index}>
-									<button
-										className='dropdown-item'
-										onClick={() => {
-											i18next.changeLanguage(country_code);
-											window.location.reload();
-										}}
-										disabled={country_code === currentLngCode}>
-										<span
-											className={`fi fi-${country_code} fis mx-3`}
-											style={{
-												opacity: country_code === currentLngCode ? 0.5 : 1,
-											}}></span>
+				<div className='container'>
+					<div class='row d-flex align-items-center'>
+						<div class='col'>
+							{/* LANGUAGES */}
+							<div className='dropdown'>
+								<a
+									className='text-reset me-3 dropdown-toggle hidden-arrow'
+									href='#'
+									id='navbarDropdownMenuLink'
+									role='button'
+									data-mdb-toggle='dropdown'
+									aria-expanded='false'>
+									<i className='fas fa-globe' style={{ color: '#000000' }}></i>
+								</a>
+								<ul
+									className='dropdown-menu dropdown-menu-end'
+									aria-labelledby='navbarDropdownMenuLink'>
+									{languages.map(({ name, country_code, index }) => {
+										return (
+											<li key={index}>
+												<button
+													className='dropdown-item'
+													onClick={() => {
+														i18next.changeLanguage(country_code);
+														window.location.reload();
+													}}
+													disabled={country_code === currentLngCode}>
+													<span
+														className={`fi fi-${country_code} fis mx-3`}
+														style={{
+															opacity:
+																country_code === currentLngCode ? 0.5 : 1,
+														}}></span>
 
-										{name}
-									</button>
-								</li>
-							);
-						})}
-					</ul>
+													{name}
+												</button>
+											</li>
+										);
+									})}
+								</ul>
+							</div>
+						</div>
+						<div class='col'>
+							<div class='d-grid gap-2'>
+								<button class='btn btn-primary' type='button'>
+									acceso
+								</button>
+							</div>
+						</div>
+					</div>
+					{/* BUTTONS */}
 				</div>
 			</div>
 		</nav>
