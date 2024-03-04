0816bd6601710b8a91eb6e7e159d20d58d972619
Abraomukas <abraomukas@gmail.com>
2024-03-04 23:33:40 +0100

    feat: Update News page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+
 function News() {
 	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100vh',
-			}}>
-			<div>CONTENT - NEWS</div>
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}>
+				CONTENT - NEWS
+			</div>
 		</div>
 	);
 }
