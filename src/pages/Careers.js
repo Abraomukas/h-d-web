051bb6cf2fddb031476fc6ea7c27434a695e6682
Abraomukas <abraomukas@gmail.com>
2024-03-06 22:50:00 +0100

    feat: Update Careers page

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+
 function Careers() {
 	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100vh',
-			}}>
-			<div>CONTENT - CAREERS</div>
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					height: '100vh',
+				}}>
+				CONTENT - CAREERS
+			</div>
 		</div>
 	);
 }
