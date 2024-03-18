d14561c1ccbc356955894003b7368c64ad37962a
Abraomukas <abraomukas@gmail.com>
2024-03-18 01:51:40 +0100

    wip: Hero content

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
+import { Trans, useTranslation } from 'react-i18next';
 
 function Hero() {
+	const currentLngCode = Cookies.get('i18next') || 'es';
+
+	const { t } = useTranslation();
+
 	return (
 		<div
 			style={{
@@ -9,31 +15,30 @@ function Hero() {
 				alignItems: 'center',
 				height: '100vh',
 			}}>
-			<div className='px-4 py-5 my-5 text-center'>
-				<img
-					className='d-block mx-auto mb-4'
-					src='/docs/5.3/assets/brand/bootstrap-logo.svg'
-					alt=''
-					width='72'
-					height='57'
-				/>
-				<h1 className='display-5 fw-bold text-body-emphasis'>Centered hero</h1>
-				<div className='col-lg-6 mx-auto'>
-					<p className='lead mb-4'>
-						Quickly design and customize responsive mobile-first sites with
-						Bootstrap, the world’s most popular front-end open source toolkit,
-						featuring Sass variables and mixins, responsive grid system,
-						extensive prebuilt components, and powerful JavaScript plugins.
-					</p>
-					<div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
-						<button type='button' className='btn btn-primary btn-lg px-4 gap-3'>
-							Primary button
-						</button>
-						<button
-							type='button'
-							className='btn btn-outline-secondary btn-lg px-4'>
-							Secondary
-						</button>
+			<div class='container col-xxl-8 px-4 py-5'>
+				<div class='row flex-lg-row-reverse align-items-center g-5 py-5'>
+					<div class='col-10 col-sm-8 col-lg-6'>
+						<img
+							src='./images/yvonne.jpeg'
+							class='d-block mx-lg-auto img-fluid'
+							alt='Bootstrap Themes'
+							width='700'
+							height='500'
+							loading='lazy'
+						/>
+					</div>
+					<div class='col-lg-6'>
+						<Trans i18nKey={'hero.heading'}>
+							<h1 class='display-5 fw-bold text-body-emphasis lh-1 mb-3'>
+								{t()}
+							</h1>
+						</Trans>
+						<p class='lead'>
+							Quickly design and customize responsive mobile-first sites with
+							Bootstrap, the world’s most popular front-end open source toolkit,
+							featuring Sass variables and mixins, responsive grid system,
+							extensive prebuilt components, and powerful JavaScript plugins.
+						</p>
 					</div>
 				</div>
 			</div>
