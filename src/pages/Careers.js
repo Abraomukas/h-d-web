9692cffbe2c8211de93377085aec97f6839e6d4a
Abraomukas <abraomukas@gmail.com>
2024-03-24 20:00:00 +0100

    feat: Add heading to Careers page

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+import { Trans, useTranslation } from 'react-i18next';
 
 //* COMPONENTS
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function Careers() {
+	const currentLngCode = Cookies.get('i18next') || 'es';
+
 	return (
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
@@ -16,7 +20,9 @@ function Careers() {
 					alignItems: 'center',
 					height: '100vh',
 				}}>
-				CONTENT - CAREERS
+				<h1 className='display-7 fw-bold text-body-emphasis lh-1 mb-3'>
+					<Trans i18nKey={'careers.heading'} />
+				</h1>
 			</div>
 
 			<Footer />
