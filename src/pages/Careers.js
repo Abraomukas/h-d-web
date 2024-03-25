b5608a2f13f6c698729c73289dc3f3e4ccabce9d
Abraomukas <abraomukas@gmail.com>
2024-03-25 20:00:00 +0100

    wip: Add open positions array

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -9,6 +9,18 @@ import Footer from '../components/Footer';
 function Careers() {
 	const currentLngCode = Cookies.get('i18next') || 'es';
 
+	const jobs = [
+		'Lead Full Stack para Java ERP con alemán',
+		'Senior/Junior Java Developer para ERP con alemán',
+		'Senior/Junior .NET Developer para RRHH con alemán',
+		'Senior/Junior Frontend Developer para RRHH con alemán',
+		'Proxy Product Owner con alemán',
+		'Software QA Manager',
+		'DevOps/sysadmin con alemán',
+		'Senior/Junior Java Developer para plataforma Synergo',
+		'Desarrollador Rust con alemán',
+	];
+
 	return (
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
