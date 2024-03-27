8ffb95182645beb356fc358820dd74db9f317ebd
Abraomukas <abraomukas@gmail.com>
2024-03-27 20:00:00 +0100

    wip: Change layout one more time

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -9,27 +9,23 @@ import Footer from '../components/Footer';
 function Careers() {
 	const currentLngCode = Cookies.get('i18next') || 'es';
 
-	const jobs = [
-		'Lead Full Stack para Java ERP con alemán',
-		'Senior/Junior Java Developer para ERP con alemán',
-		'Senior/Junior .NET Developer para RRHH con alemán',
-		'Senior/Junior Frontend Developer para RRHH con alemán',
-		'Proxy Product Owner con alemán',
-		'Software QA Manager',
-		'DevOps/sysadmin con alemán',
-		'Senior/Junior Java Developer para plataforma Synergo',
-		'Desarrollador Rust con alemán',
+	console.log('careers.jobs');
+
+	const jobLabels = [
+		'lead-full-stack',
+		'java',
+		'dot-net',
+		'frontend',
+		'product-owner',
+		'qa',
+		'devops',
+		'java-synergo',
+		'rust',
 	];
 
 	return (
 		<div style={{ position: 'relative' }}>
 			<Navbar />
-
-			<div className='container text-center'>
-				<h1 className='display-7 fw-bold text-body-emphasis lh-1 mb-3'>
-					<Trans i18nKey={'careers.heading'} />
-				</h1>
-			</div>
 			<div
 				style={{
 					display: 'flex',
@@ -38,14 +34,28 @@ function Careers() {
 					height: '100vh',
 				}}>
 				<div className='container text-center'>
-					<div className='row'></div>
-					<div className='row'>
+					<h1 className='display-7 fw-bold text-body-emphasis lh-1 mb-3'>
+						<Trans i18nKey={'careers.heading'} />
+					</h1>
+
+					<br />
+
+					<br />
+
+					<br />
+
+					<br />
+
+					<br />
+
+					<div className='container text-center'>
 						<div className='container text-center'>
 							<div className='row row-cols-3'>
-								<div className='col'>Column</div>
-								<div className='col'>Column</div>
-								<div className='col'>Column</div>
-								<div className='col'>Column</div>
+								<div className='col'>
+									<button type='button' className='btn btn-primary btn-lg'>
+										<Trans i18nKey={'careers.jobs.lead-full-stack'} />
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
