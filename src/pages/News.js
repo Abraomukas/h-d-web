e5b5a72fbc253b878d589bce63cdd02b835adfe1
Abraomukas <abraomukas@gmail.com>
2024-04-09 20:00:00 +0200

    wip: Add Header for News section

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+import { Trans, useTranslation } from 'react-i18next';
 
 //* COMPONENTS
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function News() {
+	const currentLngCode = Cookies.get('i18next') || 'es';
+
 	return (
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
 
-			<div
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					height: '100vh',
-				}}>
-				CONTENT - NEWS
+			{/* HEADER */}
+
+			<div className='position-relative p-5 p-md-1 m-md-1 text-center'>
+				<div className='col-md-6 p-lg-5 mx-auto mt-3'>
+					<h1 className='display-3 fw-bold'>
+						<Trans i18nKey={'news.heading'} />
+					</h1>
+				</div>
 			</div>
 
 			<Footer />
