98674672cec28ba418099e747ae0e8cd395d4d92
Abraomukas <abraomukas@gmail.com>
2024-05-10 23:55:00 +0200

    feat: Include translations and improve visuals

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -3,9 +3,16 @@ import Cookies from 'js-cookie';
 import { Trans, useTranslation } from 'react-i18next';
 
 import Placeholder from '../components/Placeholder';
+import FeatureEntry from '../components/FeatureEntry';
 
 function Hero() {
 	const currentLngCode = Cookies.get('i18next') || 'es';
+
+	const values = [
+		{ icon: 'fas fa-microchip', tag: 'diversity', to: '' },
+		{ icon: 'fa-solid fa-graduation-cap', tag: 'trust', to: '' },
+		{ icon: 'fa-solid fa-robot', tag: 'productivity', to: '' },
+	];
 
 	return (
 		<div
@@ -14,7 +21,6 @@ function Hero() {
 				minHeight: '100vh',
 			}}>
 			<section
-				className='justify-content-center'
 				style={{
 					backgroundColor: 'cyan',
 					minHeight: '100vh',
@@ -52,6 +58,36 @@ function Hero() {
 						</div>
 						<div className='col-10 col-sm-8 col-lg-6'>
 							<Placeholder width={500} height={400} />
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div className='container col-xxl-8 px-4 py-5'>
+					<div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
+						<div className='col-10 col-sm-8 col-lg-6'>
+							<Placeholder width={500} height={400} />
+						</div>
+
+						{/* VALUES */}
+
+						<div className='col-10 col-sm-8 col-lg-6'>
+							<div className='row row-cols-1 g-4'>
+								{values.map((value, index) => {
+									return (
+										<div>
+											<i className='fa-solid fa-robot' />
+											<h3>
+												<Trans i18nKey={`hero.values.${value.tag}`} />
+											</h3>
+											<p className='fst-italic'>
+												<Trans i18nKey={`hero.values-text.${value.tag}`} />
+											</p>
+										</div>
+									);
+								})}
+							</div>
 						</div>
 					</div>
 				</div>
