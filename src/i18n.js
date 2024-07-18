9e125f727dda227053b9d181cf7f20d5fe4587cd
Abraomukas <abraomukas@gmail.com>
2024-07-18 07:55:00 +0200

    feat: Fixed typo

--- src/i18n.js
+++ src/i18n.js
@@ -21,9 +21,8 @@ i18n
 	// for all options read: https://www.i18next.com/overview/configuration-options
 	.init({
 		// lng: document.querySelector("html").lang,
-		fallbackLng: 'es	',
+		fallbackLng: 'es',
 		debug: true,
-
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
 		},
