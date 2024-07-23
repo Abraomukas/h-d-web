2f5e243882c17ad5fc725d6b33df7aa9426e746d
Abraomukas <abraomukas@gmail.com>
2024-07-23 22:46:39 +0200

    feat: Fix layout

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -6,6 +6,7 @@ import { Link, useParams } from 'react-router-dom';
 //* COMPONENTS
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Placeholder from '../components/Placeholder';
 
 function JobDetails() {
 	const currentLngCode = Cookies.get('i18next') || 'es';
@@ -15,16 +16,37 @@ function JobDetails() {
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
 
-			<div className='container'>
+			<div className='container-fluid'>
 				{/* HEADER */}
-
 				<div className='position-relative p-5 p-md-1 m-md-1 text-center'>
 					<div className='col-md-6 p-lg-5 mx-auto mt-3'>
 						<h1 className='display-3 fw-bold'>
-							<Trans i18nKey={`careers.jobs.${job}`} />
+							<Trans i18nKey={`careers.${job}.header`} />
 						</h1>
 					</div>
 				</div>
+				{/* INTRO */}
+				<section>
+					<div className='container col-xxl-8 px-4 py-5'>
+						<div className='row d-flex justify-content-around align-items-center py-5'>
+							<div className='col-10 col-sm-8 col-lg-6'>
+								<Placeholder width={500} height={400} />
+							</div>
+							<div className='col-lg-6'>
+								<h1 className='display-5 fw-bold text-body-emphasis lh-1 mb-3'>
+									<Trans i18nKey={'hero.header'} />
+								</h1>
+								<p className='lead'>
+									<Trans i18nKey={'hero.subheader'} />
+								</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				{/* STACK */}
+				{/* SOFT SKILLS */}
+				{/* BENEFITS */}
+				{/* SEND ME YOUR CV */}
 			</div>
 
 			<Footer />
