b5a4cc77abd56ef5a5ebe09d4f2a4cffe8543149
Abraomukas <abraomukas@gmail.com>
2024-11-07 01:19:28 +0100

    feat: add new language to toggle - en

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ import { Trans, useTranslation } from 'react-i18next';
 const languages = [
 	{ name: 'Español', country_code: 'es' },
 	{ name: 'Deutsch', country_code: 'de' },
+	{ name: 'English', country_code: 'gb' },
 ];
 
 const sections = [
@@ -16,7 +17,138 @@ const sections = [
 	{ label: 'pages.careers', to: '/careers' },
 ];
 
-function Navbar(props) {
+// function Navbar() {
+// 	const [isDarkMode, setIsDarkMode] = useState(false);
+
+// 	const currentLngCode = Cookies.get('i18next') || 'es';
+
+// 	const { t } = useTranslation();
+
+// 	return (
+// 		<nav
+// 			className={`navbar navbar-expand-lg navbar-${
+// 				isDarkMode ? 'dark' : 'light'
+// 			} bg-${
+// 				isDarkMode ? 'dark' : 'light'
+// 			} h-100 d-flex justify-content-between align-items-center`}>
+// 			<div className='navbar-left'>
+// 				{/* BRAND */}
+// 				<div className='navbar-middle d-flex align-items-center mx-auto'>
+// 					<a href='/'>
+// 						<img
+// 							src='./images/logo.png'
+// 							height='80'
+// 							alt='H&D Logo'
+// 							loading='lazy'
+// 						/>
+// 					</a>
+// 				</div>
+// 			</div>
+// 			<div className='navbar-middle'>
+// 				{/* LINKS */}
+// 				<button
+// 					className='navbar-brand navbar-toggler'
+// 					type='button'
+// 					data-mdb-toggle='collapse'
+// 					data-mdb-target='#navbarSupportedContent'
+// 					aria-controls='navbarSupportedContent'
+// 					aria-expanded='false'
+// 					aria-label='Toggle navigation'>
+// 					<i className='fas fa-bars'></i>
+// 				</button>
+// 				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+// 					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+// 						{/* SECTIONS */}
+// 						{sections.map(({ label, to }, index) => {
+// 							return (
+// 								<li key={index} className='nav-item m-2'>
+// 									<Trans i18nKey={`${label}`}>
+// 										<Link key={index} className='nav-link' to={to}>
+// 											{t(label)}
+// 										</Link>
+// 									</Trans>
+// 								</li>
+// 							);
+// 						})}
+// 					</ul>
+// 				</div>
+// 			</div>
+// 			<div className='navbar-right'>
+// 				<div className='container'>
+// 					<div className='row d-flex align-items-center'>
+// 						<div className='col'>
+// 							{/* DARK MODE */}
+// 							<div className='dropdown'>
+// 								<a
+// 									className='text-reset me-3'
+// 									href='#'
+// 									role='button'
+// 									aria-expanded='false'
+// 									onClick={() => {
+// 										setIsDarkMode(!isDarkMode);
+// 									}}>
+// 									{isDarkMode ? (
+// 										<i className='fas fa-sun' style={{ color: '#ffffff' }} />
+// 									) : (
+// 										<i className='fas fa-moon' style={{ color: '#000000' }} />
+// 									)}
+// 								</a>
+// 							</div>
+// 						</div>
+// 						<div className='col'>
+// 							{/* LANGUAGES */}
+// 							<div className='dropdown'>
+// 								<a
+// 									className='text-reset me-3 dropdown-toggle hidden-arrow'
+// 									href='#'
+// 									id='navbarDropdownMenuLink'
+// 									role='button'
+// 									data-mdb-toggle='dropdown'
+// 									aria-expanded='false'>
+// 									<i
+// 										className='fas fa-globe'
+// 										style={{ color: isDarkMode ? '#ffffff' : '#000000' }}></i>
+// 								</a>
+// 								<ul
+// 									className='dropdown-menu dropdown-menu-end'
+// 									aria-labelledby='navbarDropdownMenuLink'>
+// 									{languages.map(({ name, country_code }, index) => {
+// 										return (
+// 											<li key={index}>
+// 												<button
+// 													key={index}
+// 													className='dropdown-item'
+// 													onClick={() => {
+// 														i18next.changeLanguage(country_code);
+// 														window.location.reload();
+// 													}}
+// 													disabled={country_code === currentLngCode}>
+// 													<span
+// 														key={index}
+// 														className={`fi fi-${country_code} fis mx-3`}
+// 														style={{
+// 															opacity:
+// 																country_code === currentLngCode ? 0.5 : 1,
+// 														}}
+// 													/>
+
+// 													{name}
+// 												</button>
+// 											</li>
+// 										);
+// 									})}
+// 								</ul>
+// 							</div>
+// 						</div>
+// 					</div>
+// 					{/* BUTTONS */}
+// 				</div>
+// 			</div>
+// 		</nav>
+// 	);
+// }
+
+export default function Navbar() {
 	const [isDarkMode, setIsDarkMode] = useState(false);
 
 	const currentLngCode = Cookies.get('i18next') || 'es';
@@ -25,26 +157,22 @@ function Navbar(props) {
 
 	return (
 		<nav
-			className={`navbar navbar-expand-lg navbar-${
+			className={`navbar navbar-expand-lg d-flex navbar-${
 				isDarkMode ? 'dark' : 'light'
-			} bg-${
-				isDarkMode ? 'dark' : 'light'
-			} h-100 d-flex justify-content-between align-items-center`}>
-			<div className='navbar-left'>
-				{/* BRAND */}
-				<div className='navbar-middle d-flex align-items-center mx-auto'>
-					<a href='/'>
-						<img
-							src='./images/logo.png'
-							height='80'
-							alt='H&D Logo'
-							loading='lazy'
-						/>
-					</a>
-				</div>
+			} bg-${isDarkMode ? 'dark' : 'light'}`}>
+			{/* BRAND */}
+			<div className='d-flex align-items-center'>
+				<a href='/'>
+					<img
+						src='./images/logo.png'
+						height='80'
+						alt='H&D Logo'
+						loading='lazy'
+					/>
+				</a>
 			</div>
-			<div className='navbar-middle'>
-				{/* LINKS */}
+			{/* LINKS */}
+			<div className='mx-auto'>
 				<button
 					className='navbar-brand navbar-toggler'
 					type='button'
@@ -57,7 +185,6 @@ function Navbar(props) {
 				</button>
 				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
 					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-						{/* SECTIONS */}
 						{sections.map(({ label, to }, index) => {
 							return (
 								<li key={index} className='nav-item m-2'>
@@ -72,11 +199,11 @@ function Navbar(props) {
 					</ul>
 				</div>
 			</div>
-			<div className='navbar-right'>
+			{/* DARK MODE && LANGUAGES */}
+			<div>
 				<div className='container'>
 					<div className='row d-flex align-items-center'>
 						<div className='col'>
-							{/* DARK MODE */}
 							<div className='dropdown'>
 								<a
 									className='text-reset me-3'
@@ -95,7 +222,6 @@ function Navbar(props) {
 							</div>
 						</div>
 						<div className='col'>
-							{/* LANGUAGES */}
 							<div className='dropdown'>
 								<a
 									className='text-reset me-3 dropdown-toggle hidden-arrow'
@@ -140,11 +266,8 @@ function Navbar(props) {
 							</div>
 						</div>
 					</div>
-					{/* BUTTONS */}
 				</div>
 			</div>
 		</nav>
 	);
-}
-
-export default Navbar;
+};
