fb53aff5b376ad65fb8d8b64a19a3b5d6f630555
Abraomukas <abraomukas@gmail.com>
2024-12-03 22:25:00 +0100

    feat: update Hero section sizes

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 import { Trans, useTranslation } from 'react-i18next';
+import { Novatrix } from 'uvcanvas';
 
 import Placeholder from '../components/Placeholder';
 
@@ -18,32 +19,14 @@ function Hero() {
 	return (
 		<div style={{ minHeight: '100vh' }}>
 			{/* COVER IMAGE WITH MESSAGE */}
-			<section
-				style={{
-					minHeight: '100vh',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-				}}>
+			<section style={{ minHeight: '100vh' }}>
 				<div className='container col-xxl-8 px-4 py-5'>
-					<div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
-						<div className='col-10 col-sm-8 col-lg-6'>
-							<Placeholder width={700} height={400} />
-						</div>
-						<div className='col-lg-6'>
-							<h1 className='display-5 fw-bold text-body-emphasis lh-1 mb-3'>
-								<Trans i18nKey={'hero.header'} />
-							</h1>
-							<p className='lead'>
-								<Trans i18nKey={'hero.subheader'} />
-							</p>
-						</div>
-					</div>
+					<Novatrix />
 				</div>
 			</section>
 
 			{/* TIMELINE */}
-			<section>
+			<section style={{ minHeight: '100vh' }}>
 				<div className='container-fluid px-5 pb-5'>
 					<div className='row'>
 						<div className='container-fluid text-center'>
@@ -69,7 +52,9 @@ function Hero() {
 			</section>
 
 			{/* CTA for Services */}
-			<section></section>
+			<section style={{ minHeight: '100vh' }}>
+				<Novatrix />
+			</section>
 		</div>
 	);
 }
