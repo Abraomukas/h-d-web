faccc608730204417739991566e646210689a273
Abraomukas <abraomukas@gmail.com>
2024-12-17 23:15:00 +0000

    feat: fix timeline layout

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -4,73 +4,86 @@ import { auto } from "@cloudinary/url-gen/actions/resize";
 import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 import Cookies from "js-cookie";
 import { Trans, useTranslation } from "react-i18next";
+import TimelineEntry from '../components/TimelineEntry';
 
 export default function Hero() {
-  const cld = new Cloudinary({ cloud: { cloudName: "abraomukas" } });
+	const cld = new Cloudinary({ cloud: { cloudName: 'abraomukas' } });
 
-  const currentLngCode = Cookies.get("i18next") || "es";
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  const imageUrl = cld
-    .image("h-d-web/jan-24")
-    .format("auto")
-    .quality("auto")
-    .resize(auto().gravity(autoGravity()).width(width).height(height))
-    .toURL();
+	const currentLngCode = Cookies.get('i18next') || 'es';
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	const imageUrl = cld
+		.image('h-d-web/jan-24')
+		.format('auto')
+		.quality('auto')
+		.resize(auto().gravity(autoGravity()).width(width).height(height))
+		.toURL();
 
-  useTranslation();
+	useTranslation();
 
-  const values = [
-    { icon: "fa-solid fa-person", tag: "diversity", to: "" },
-    { icon: "fa-solid fa-users", tag: "trust", to: "" },
-    { icon: "fa-solid fa-stopwatch", tag: "productivity", to: "" },
-  ];
+	const timeline = [
+		{ icon: 'fas fa-globe', tag: 'since', img: './images/AKDB.png' },
+		{
+			icon: 'fa-solid fa-recycle',
+			tag: 'innovate',
+			img: './images/HyD-Munich.png',
+		},
+		{
+			icon: 'fa-solid fa-user-group',
+			tag: 'together',
+			img: './images/HyD-Spain.png',
+		},
+	];
 
-  return (
-    <div>
-      {/* COVER IMAGE WITH MESSAGE */}
-      <section>
-        <div className="container-fluid px-5 py-5">
-          <img src={imageUrl} className="img-fluid" />
-        </div>
-      </section>
+	return (
+		<div>
+			{/* COVER IMAGE WITH MESSAGE */}
+			<section>
+				<div className='container-fluid px-5 py-5'>
+					<img src={imageUrl} className='img-fluid' />
+				</div>
+			</section>
 
-      {/* TIMELINE */}
-      <section>
-        <div
-          style={{ minHeight: "100vh" }}
-          className="container-fluid px-5 pb-5"
-        >
-          <div className="row">
-            <div className="container-fluid text-center">
-              <h1 className="display-7 fw-bold lh-1 mb-3">
-                <Trans i18nKey={"hero.timeline-header"} />
-              </h1>
-            </div>
-            <div className="row ">
-              <div
-                className="col-lg-4 col-md-12"
-                style={{ backgroundColor: "red" }}
-              ></div>
+			{/* TIMELINE */}
+			<section>
+				<div className='container px-5 pb-5'>
+					<div className='row'>
+						<div className='container-fluid text-center'>
+							<h1 className='display-7 fw-bold lh-1 mb-5'>
+								<Trans i18nKey={'hero.timeline-header'} />
+							</h1>
+						</div>
+						<div className='row grid gap-1'>
+							{timeline.map((entry, index) => {
+								return (
+									<div
+										index={index}
+										className='col-4 border'
+										style={{
+											backgroundColor: 'gray',
+											minWidth: '300px',
+											maxWidth: '33%',
+										}}>
+										<div index={index} className='container my-5'>
+											<TimelineEntry
+												index={index}
+												icon={entry.icon}
+												tag={entry.tag}
+												img={entry.img}
+											/>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			</section>
 
-              <div
-                className="col-lg-4 col-md-6"
-                style={{ backgroundColor: "green" }}
-              ></div>
-
-              <div
-                className="col-lg-4 col-md-6"
-                style={{ backgroundColor: "blue", minHeight: "100vh" }}
-              ></div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* CTA for Services */}
-      <section>
-        <div style={{ minHeight: "100vh" }}></div>
-      </section>
-    </div>
-  );
+			{/* CTA for Services */}
+			<section>
+				<div style={{ minHeight: '100vh' }}></div>
+			</section>
+		</div>
+	);
 }
