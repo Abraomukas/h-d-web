7b660925635cc9c50f359be0d3aefbff2866686e
Abraomukas <abraomukas@gmail.com>
2024-12-26 23:59:58 +0000

    feat: new structure for Services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Cookies from 'js-cookie';
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
 
 //* COMPONENTS
 import Navbar from '../components/Navbar';
@@ -9,7 +12,17 @@ import Placeholder from '../components/Placeholder';
 import FeatureEntry from '../components/FeatureEntry';
 
 function Services() {
+	const cld = new Cloudinary({ cloud: { cloudName: 'abraomukas' } });
+
 	const currentLngCode = Cookies.get('i18next') || 'es';
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	const imageUrl = cld
+		.image('h-d-web/jan-24')
+		.format('auto')
+		.quality('auto')
+		.resize(auto().gravity(autoGravity()).width(width).height(height))
+		.toURL();
 
 	const services = [
 		{ icon: 'fas fa-microchip', text: 'tech', to: '' },
@@ -25,59 +38,61 @@ function Services() {
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
 
+			{/* COVER IMAGE */}
 			<section>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						height: '100vh',
-					}}>
-					<div
-						className='container-fluid'
-						style={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							height: '100vh',
-						}}>
-						<div className='row gx-1'>
-							<div className='col-lg-2 col-md-1' />
-							<div className='col-lg-4 col-md-5'>
-								<div className='container'>
-									<h1 className='display-2 fw-bold'>
-										<Trans i18nKey={'services.header'} />
-									</h1>
+				<div className='container-fluid px-5 py-5'>
+					<img
+						style={{ objectFit: 'cover' }}
+						src={imageUrl}
+						className='img-fluid'
+						alt='Background'
+					/>
+				</div>
 
-									<br />
-
-									<br />
-
-									<br />
-
-									<h3>
-										<Trans i18nKey={'services.subheader'} />
-									</h3>
-								</div>
-							</div>
-							<div className='col-lg-4 col-md-5'>
-								<div
-									className='bg-image hover-overlay ripple shadow-2-strong rounded-3'
-									data-mdb-ripple-color='light'>
-									<Placeholder width={600} height={700} />
-								</div>
-							</div>
-							<div className='col-lg-2 col-md-1' />
+				<div className='container px-5	'>
+					<div className='row'>
+						<div className='container-fluid text-center'>
+							<h1 className='display-7 fw-bold lh-1 mb-5'>
+								<Trans i18nKey={'services.header'} />
+							</h1>
+							<p className='fs-4'>
+								<Trans i18nKey={'services.subheader'} />
+							</p>
 						</div>
 					</div>
 				</div>
 			</section>
 
+			{/* TEAMS */}
 			<section>
-				<div className='position-relative p-5 p-md-1 m-md-1 text-center'>
+				<div className='position-relative p-md-1 m-md-1 text-center'>
 					<div className='col-md-6 p-lg-5 mx-auto mt-3'>
-						<h1 className='display-3 fw-bold'>
-							<Trans i18nKey={'services.features-header'} />
+						<h1 className='display-5 fw-bold'>
+							<Trans i18nKey={'services.our-teams'} />
+						</h1>
+					</div>
+				</div>
+
+				<div className='container mb-7'>
+					<div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5'>
+						{services.map((service, index) => {
+							return (
+								<FeatureEntry
+									index={index}
+									icon={service.icon}
+									text={`services.features.${service.text}`}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div className='position-relative p-md-1 m-md-1 text-center'>
+					<div className='col-md-6 p-lg-5 mx-auto mt-3'>
+						<h1 className='display-5 fw-bold'>
+							<Trans i18nKey={'services.our-teams'} />
 						</h1>
 					</div>
 				</div>
