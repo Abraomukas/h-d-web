e00073c19162ca1995c5b4c00ceed9ee81e87215
Abraomukas <abraomukas@gmail.com>
2024-12-30 22:26:49 +0000

    feat: add TeamEntry.js

---
+++ src/components/TeamEntry.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Cookies from 'js-cookie';
+import { Trans } from 'react-i18next';
+
+function TeamEntry(props) {
+	const currentLngCode = Cookies.get('i18next') || 'es';
+
+	return (
+		<div className='col'>
+			<div className='card text-center' style={{ width: '18rem' }}>
+				<div className='card-body'>
+					<h5 className='card-title'>
+						<Trans i18nKey={`services.teams-names.${props.team}`} />
+					</h5>
+					<p className='card-text'>
+						<Trans i18nKey={`services.teams-descriptions.${props.team}`} />
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default TeamEntry;
